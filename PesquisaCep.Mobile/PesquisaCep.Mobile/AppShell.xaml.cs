2b9d96e972c2c128c906de624dcb1ac789bf762c
Jonathan Braga <jonathan.braga@sibs.com>
2020-10-03 01:11:20 +0100

    frist commit

---
+++ PesquisaCep.Mobile/PesquisaCep.Mobile/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using PesquisaCep.Mobile.ViewModels;
+using PesquisaCep.Mobile.Views;
+using Xamarin.Forms;
+
+namespace PesquisaCep.Mobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ PesquisaCep.Mobile/PesquisaCep.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using PesquisaCep.Mobile.ViewModels;
+
+namespace PesquisaCep.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
